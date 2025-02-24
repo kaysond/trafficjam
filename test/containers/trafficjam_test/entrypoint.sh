4cd6d80e6385665281cd55df43d5cbcc024c99dc
Aram Akhavan <github@aram.nubmail.ca>
2025-02-24 13:58:38 -0500

    Improve CI repeatability on GH Runners (#24)
    * Fix test teardown function
    
    * Dump logs if dind startup times out
    
    * Increase timeouts again

--- test/containers/trafficjam_test/entrypoint.sh
+++ test/containers/trafficjam_test/entrypoint.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 /usr/local/bin/dockerd-entrypoint.sh dockerd &
-#Wait for docker startup for 60s
-while ! docker ps; do
-    if (( ++i > 12 )); then
+# Wait for docker daemon to start up so we can build the containers
+while ! docker ps &> /dev/null; do
+    if (( ++i > 24 )); then
         echo "Timed out waiting for docker to start up" >&2
         exit 1
     fi
